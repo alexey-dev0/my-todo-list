ff50402c1d18ed70466e15924fcc570662b6b530
Алексей Мошкин <a.moshkin@zonatelecom.ru>
2023-11-01 18:43:09 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ChakraProvider } from '@chakra-ui/react';
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <div>Hello, world!</div>
+    </ChakraProvider>
+  )
+}
+
+export default App
